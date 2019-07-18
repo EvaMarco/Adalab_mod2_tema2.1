b985590e9c5a4289700540b9e449e273ae800f52
Migara <evamarcod@gmail.com>
2019-07-18 09:06:12 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,3 @@
+'use strict';
+const newTitle = document.querySelector('.title');
+newTitle.innerHTML = 'Esta página no es compatible con la versión actual de tu navegador. Por favor actualize su navedador';
