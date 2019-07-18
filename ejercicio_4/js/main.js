f9765173b9b6f71070a9bf5fdae6e9715d718d96
Constanza Sosa <constanza.sosa.g@gmail.com>
2019-07-18 09:37:48 +0200

    Done ex 4

---
+++ ejercicio_4/js/main.js
@@ -0,0 +1,6 @@
+'use strict';
+
+const adalaber = document.querySelector('.title');
+const member =document.querySelector('.list-item:nth-child(2)');
+
+adalaber.innerHTML = adalaber.innerHTML + ' ' + member.innerHTML;
